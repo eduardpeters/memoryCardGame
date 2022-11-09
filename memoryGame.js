bc2b49ec2e3af9c074b166d4d7f41846c43d2d99
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2022-11-09 10:55:25 +0100

    Updating project files from shared repository

--- memoryGame.js
+++ memoryGame.js
@@ -10,143 +10,139 @@ const gameState = {
     currentCards: []
 };
 
-function initializeGame(){
+function initializeGame() {
     const playerOneName = document.getElementById("playerOneInput").value;
     const playerTwoName = document.getElementById("playerTwoInput").value;
     const numberOfCards = parseInt(document.getElementById("level").value);
-    console.log(numberOfCards);
-    if (validateInput(playerOneName, playerTwoName, numberOfCards)){
-        inputUser (playerOneName, playerTwoName);
-        updateScoreBoard();
+    if (validateInput(playerOneName, playerTwoName, numberOfCards)) {
+        inputUser(playerOneName, playerTwoName);
         gameState.maxPoints = numberOfCards / 2;
         placeBoard(numberOfCards);
+        document.getElementById("userForm").style.display = "none";
+        document.getElementById("gameScreen").style.display = "block";
+
     }
 }
 
-function inputUser(playerOneName, playerTwoName){ 
+function inputUser(playerOneName, playerTwoName) {
     document.getElementById("playerOneName").innerHTML = playerOneName;
     document.getElementById("playerTwoName").innerHTML = playerTwoName;
     gameState.playerOneName = playerOneName;
     gameState.playerTwoName = playerTwoName;
+    updateScore();
 }
 
-function updateScoreBoard (){
+function updateScore() {
     document.getElementById("playerOneScore").innerHTML = gameState.playerOneScore;
     document.getElementById("playerTwoScore").innerHTML = gameState.playerTwoScore;
-    if (gameState.playerOneTurn)
-        document.getElementById("currentTurn").innerHTML = gameState.playerOneName;
-    else
-        document.getElementById("currentTurn").innerHTML = gameState.playerTwoName;
 }
 
-
-function validateInput(playerOneName, playerTwoName, numberOfCards){
-    if (playerOneName.length < 3 || playerTwoName.length < 3){
+function validateInput(playerOneName, playerTwoName, numberOfCards) {
+    if (playerOneName.length < 3 || playerTwoName.length < 3) {
         alert("Player names must be at least three characters");
         return (false);
     }
-    if (!numberOfCards){
+    if (!numberOfCards) {
         alert("Select how many cards to play with");
         return (false);
     }
-    if (numberOfCards < 12 || numberOfCards > 48 || numberOfCards % 2 !== 0){
+    if (numberOfCards < 12 || numberOfCards > 48 || numberOfCards % 2 !== 0) {
         alert("Not a valid number of cards");
         return (false);
     }
     return (true);
 }
 
-function placeBoard(numberOfCards){
+function placeBoard(numberOfCards) {
     const boardOrder = randomOrderArray(numberOfCards);
     const parent = document.getElementById("board");
     console.log(boardOrder);
     boardOrder.forEach(pairValue => {
         const newDiv = document.createElement("div");
+        const newP = document.createElement("p");
         newDiv.className = "card";
         newDiv.dataset.value = pairValue;
-        newDiv.dataset.faceUp = 0;
-        newDiv.dataset.taken = 0;
         newDiv.addEventListener("click", handleCardClick);
+        newDiv.appendChild(newP);
         parent.appendChild(newDiv);
     });
 }
 
-function randomOrderArray(numberOfCards){
-    const pairsArray = [];
+function randomOrderArray(numberOfCards) {
     const randomArray = [];
     for (let i = 0; i < numberOfCards / 2; i++)
-        pairsArray.push(i, i);
-    for (let i = 0; i < numberOfCards; i++){
-        const randomIndex = Math.floor(Math.random() * pairsArray.length);
-        randomArray.push(pairsArray[randomIndex]);
-        pairsArray.splice(randomIndex, 1);
+        randomArray.push(i, i);
+    for (let i = randomArray.length - 1; i > 0; i--) {
+        const randomIndex = Math.floor(Math.random() * (i + 1));
+        [randomArray[i], randomArray[randomIndex]] = [randomArray[randomIndex], randomArray[i]];
     }
     return (randomArray);
 }
 
-function handleCardClick(event){
+function handleCardClick(event) {
     const currentCard = event.target;
-    if (currentCard.dataset.faceUp === "0" && gameState.currentCards.length < 2){
-        gameState.currentCards.push(currentCard.dataset.value);
-        currentCard.innerHTML = currentCard.dataset.value;
-        currentCard.dataset.faceUp = 1;
-        if (gameState.currentCards.length === 2) {
+    if (gameState.currentCards.indexOf(currentCard) === -1 && gameState.currentCards.length < 2) {
+        gameState.currentCards.push(currentCard);
+        currentCard.classList.add("cardFlipping");
+        setTimeout(() => {
+            currentCard.classList.remove("cardFlipping");
+            currentCard.children[0].innerHTML = currentCard.dataset.value
+        }, 300);
+        if (gameState.currentCards.length === 2)
             setTimeout(checkOutcome, 1500);
-        }
-        console.log(gameState);
     }
 }
 
-function checkOutcome(){
-    console.log("Decide what should happen");
-    if (gameState.currentCards[0] === gameState.currentCards[1]){
-        if (gameState.playerOneTurn)
-            gameState.playerOneScore++;
-        else
-            gameState.playerTwoScore++;
+function checkOutcome() {
+    if (gameState.currentCards[0].dataset.value === gameState.currentCards[1].dataset.value) {
+        gameState.playerOneTurn ? gameState.playerOneScore++ : gameState.playerTwoScore++;
         removeCardVisibility();
+        updateScore();
     }
     else {
         gameState.playerOneTurn = !gameState.playerOneTurn;
         revertCardFaces();
     }
     gameState.currentCards = [];
-    updateScoreBoard();
     if (gameState.playerOneScore + gameState.playerTwoScore === gameState.maxPoints)
         endGame();
 }
 
-function removeCardVisibility(){
-    const cardsArray = document.getElementsByClassName("card");
-    for (let card of cardsArray){
-        if (card.dataset.faceUp === "1"){
+function endGame() {
+    if (gameState.playerOneScore === gameState.playerTwoScore) {
+        document.getElementById("winner").innerHTML = "Empate";
+    }
+    else {
+        const finalResult = gameState.playerOneScore > gameState.playerTwoScore ? gameState.playerOneName : gameState.playerTwoName;
+        document.getElementById("winner").innerHTML = `${finalResult} ha ganado la partida`;
+    }
+    document.getElementById("gameScreen").style.display = "none";
+    document.getElementById("endScreen").style.display = "block";
+}
+
+function removeCardVisibility() {
+    gameState.currentCards.forEach(card => {
+        card.classList.add("cardFading");
+        setTimeout(() => {
             card.dataset.faceUp = 0;
             card.style.visibility = "hidden";
-        }
-    };
+            card.classList.remove("cardFading");
+        }, 300);
+    });
 }
 
-function revertCardFaces(){
-    const cardsArray = document.getElementsByClassName("card");
-    console.log(cardsArray);
-    for (let card of cardsArray){
-        if (card.dataset.faceUp === "1"){
-            card.innerHTML = "";
+function revertCardFaces() {
+    gameState.currentCards.forEach(card => {
+        card.classList.add("cardFlipping");
+        setTimeout(() => {
+            card.children[0].innerHTML = "";
             card.dataset.faceUp = 0;
-        }
-    };
+            card.classList.remove("cardFlipping");
+        }, 150);
+    });
 }
 
-function endGame(){
-    if (gameState.playerOneScore === gameState.playerTwoScore)
-        alert("It's a tie!");
-    else {
-        const winner = gameState.playerOneScore > gameState.playerTwoScore ? gameState.playerOneName : gameState.playerTwoName;
-        alert(`${winner} wind this game!`)
-    }
-}
-
-function resetGame(){
+function resetGame() {
     console.log("Should be reset");
     window.location.reload();
 }
